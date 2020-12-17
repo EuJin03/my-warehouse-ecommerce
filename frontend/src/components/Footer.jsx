a04d48d84bedb44e47656f4e506deb6d93beb647
EuJin03 <eugenetin99@gmail.com>
2020-12-17 09:17:19 +0800

    UI designing and routing

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+const Footer = () => {
+  const year = new Date().getFullYear();
+
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className="text-center py-3">
+            Copyright &copy; WareHouse {year}
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
