276d4732bbbb151f2ef27bee51a954e0f41eb64c
EuJin03 <eugenetin99@gmail.com>
2020-12-27 07:13:41 +0800

    bug fixes 0.0.1

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ const __dirname = path.resolve(); // mimic the __dirname directory
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static("/frontend/build"));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
